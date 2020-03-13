d8bcf6e27522d1f651b686371a986d350398337f
bill-fultonii <58749394+bill-fultonii@users.noreply.github.com>
2020-03-13 14:16:53 -0400

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,5 +1,8 @@
 class profile::base {
   file {'/usr/java/':
     ensure => 'directory'
-  }  
+  }
+  file {'/usr/java/jdk-13.0.2_linux-x64_bin.tar.gz':
+    ensure => present,
+    source => '/polk/nas/repo/jdk-13.0.2_linux-x64_bin.tar.gz'
 }
